3111d181bcb0345f3ca93e49b6613207df73e3d2
LFLATH <ludwiglechtreck@gmail.com>
2022-04-15 23:36:53 -0400

    Completed the functionality for indeed to scrape working urls for job postings based on indeed urls.

--- indeed.py
+++ indeed.py
@@ -1,12 +1,23 @@
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 import requests
 
 class indeedScrape():
     def __init__(self, job_title, location, radius, experience):
-        self.url = "https://www.indeed.com/jobs?q="+job_title+"&l="+ location+"%2C%20GA&radius="+radius+"&explvl="+experience+"vjk=1a0d0adde0c5fd5d"
+        self.url = "https://www.indeed.com/jobs?q="+job_title+"&l="+ location+"&radius="+radius+"&explvl="+experience
+    def createLinks(self, links):
+        for link in links:
+            print("https://www.indeed.com"+link)
+    def cleanLinks(self, links):
+        useful_urls = []
+        for link in links:
+            if "pagead" in str(link):
+                useful_urls.append(str(link))
+        self.createLinks(useful_urls)
     def scrape(self):
+        scraped_links = []
         source = requests.get(self.url)
-        soup = BeautifulSoup(source.text, "lxml")
-        jobs = soup.find_all("h2")
-        for i in range(len(jobs)): 
-            print(jobs[i])
+        for link in BeautifulSoup(source.text,'html.parser', parse_only = SoupStrainer('a')):
+            if link.has_attr('href'):
+                  scraped_links.append(link['href'])
+        self.cleanLinks(scraped_links)
